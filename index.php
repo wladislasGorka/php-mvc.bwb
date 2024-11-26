c6c508b875e292e3937fc937465f8968833eae6e
wladislas.gorka <wladislas.gorka@gmail.com>
2024-11-26 10:30:33 +0100

    mise en place

--- index.php
+++ index.php
@@ -6,6 +6,18 @@
     <title>php-mvc</title>
 </head>
 <body>
-    
+    <?php
+        spl_autoload_register(function ($class) {
+            if(file_exists('./core/' . $class . '.php')):
+                include './core/' . $class . 'php';
+            elseif(file_exists('./models/'. $class . 'php')):
+                include './models/' . $class . 'php';
+            elseif(file_exists('./controllers/'. $class . 'php')):
+                include './controllers/' . $class . 'php';
+            endif;
+        });
+    ?>
+
+    <h1>Hello</h1>
 </body>
 </html>
